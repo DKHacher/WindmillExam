d74b278f13e9a45bb5eac67fc1534216bea7163d
David Ch <raulchuchuca951@gmail.com>
2026-02-23 11:08:56 +0100

    WindmillDTO and related entities added

---
+++ Server/DTOs/WindmillDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Server.DTOs;
+
+public class WindmillDTO
+{
+    public int windmillId { get; set; }
+    public string alertType { get; set; }
+    public DateTime triggeredAt { get; set; }
+    public string messagge { get; set; }
+}
