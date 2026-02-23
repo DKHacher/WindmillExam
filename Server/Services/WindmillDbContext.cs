68f543c84c2de1c0f7ccaa10b968d66fe412b1ee
DKHacher <146067965+DKHacher@users.noreply.github.com>
2026-02-23 10:54:26 +0100

    adding dbcontext

---
+++ Server/Services/WindmillDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Server.Services;
+
+public class WindmillDbContext : DbContext
+{
+    public WindmillDbContext(DbContextOptions<WindmillDbContext> options)
+        : base(options)
+    {
+    }
+
+    //public DbSet<Telemetry> Telemetries => Set<Telemetry>(); //TODO: add entities and dbsets of those entities 
+}
