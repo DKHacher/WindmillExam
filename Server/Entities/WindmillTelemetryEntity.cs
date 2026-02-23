f5ae44665bbcb625477685efb2718fc21c781377
DKHacher <146067965+DKHacher@users.noreply.github.com>
2026-02-23 11:36:26 +0100

    added migrations and database

--- Server/Entities/WindmillTelemetryEntity.cs
+++ Server/Entities/WindmillTelemetryEntity.cs
@@ -2,6 +2,7 @@
 
 public class WindmillTelemetryEntity
 {
+    public Guid Id { get; set; }
     public string turbineId { get; set; }
     public string turbineName { get; set; }
     public string farmId { get; set; }
