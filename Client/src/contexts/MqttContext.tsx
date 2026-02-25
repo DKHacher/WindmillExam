37776342cb372c13269d7889368bc58e7c6dd0fa
thoknu <144660048+thoknu@users.noreply.github.com>
2026-02-25 06:55:31 +0100

    added mqtt plumbing in frontend

---
+++ Client/src/contexts/MqttContext.tsx
@@ -0,0 +1,12 @@
+﻿import { createContext, useContext } from "react";
+import { MqttClient } from "mqtt";
+
+type MqttContextType = {
+    mqttClient: MqttClient | null;
+}
+
+export const MqttContext = createContext<MqttContextType>({
+    mqttClient: null
+});
+
+export const useMqtt = () => useContext(MqttContext);
---
+++ Client/src/types/telemetry.ts
@@ -0,0 +1,17 @@
+﻿export type Telemetry = {
+    turbineId: string;
+    turbineName: string;
+    farmId: string;
+    timestamp: Date;
+    windSpeed: number;
+    windDirection: number;
+    ambientTemperature: number;
+    rotorSpeed: number;
+    powerOutput: number;
+    nacelleDirection: number;
+    bladePitch: number;
+    generatorTemp: number;
+    gearboxTemp: number;
+    vibration: number;
+    status: string;
+};
