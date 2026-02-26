71e2c47c4a415f5382420677f825b883736b842e
David Ch <raulchuchuca951@gmail.com>
2026-02-26 10:05:03 +0100

    Update: Controllers refactored to unify functions and use Mqtt

--- Server/Controllers/WindmillController.cs
+++ Server/Controllers/WindmillController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Server.DTOs;
 using Server.Entities;
 using Server.Services;
@@ -7,16 +8,91 @@ using StateleSSE.AspNetCore.EfRealtime;
 
 namespace Server.Controllers;
 
-[ApiController]
-[Route("api/[controller]")]
-public class WindmillController(ISseBackplane backplane, WindmillDbContext _ctx, IRealtimeManager realtimeManager)
-    : RealtimeControllerBase(backplane)
+public class WindmillController : RealtimeControllerBase
 {
-    private readonly WindmillDbContext _context = _ctx;
-    private readonly IRealtimeManager _realtimeManager = realtimeManager;
+    private readonly WindmillDbContext _ctx;
+    private readonly IRealtimeManager _realtime;
 
-    [HttpPost]
-    public async Task<IActionResult> AddTelemetry(WindmillTelemetryDTO telemetry)
+    public WindmillController(
+        ISseBackplane backplane,
+        WindmillDbContext ctx,
+        IRealtimeManager realtime)
+        : base(backplane)
+    {
+        this._ctx = ctx;
+        _realtime = realtime;
+    }
+    
+    [HttpGet("telemetry-realtime")]
+    public async Task<RealtimeListenResponse<List<WindmillTelemetryDTO>>> GetTelemetries(
+        string connectionId)
+    {
+        var group = "stats:windmill:telemetry";
+
+        
+        await Backplane.Groups.AddToGroupAsync(connectionId, group);
+
+        
+        _realtime.Subscribe<WindmillDbContext>(
+            connectionId,
+            group,
+            criteria: snapshot =>
+                snapshot.HasChanges<WindmillTelemetryEntity>(),
+
+            query: async ctx =>
+                await ctx.Telemetries
+                    .OrderByDescending(t => t.timestamp)
+                    .Take(50)
+                    .Select(t => new WindmillTelemetryDTO
+                    {
+                        turbineId = t.turbineId,
+                        turbineName = t.turbineName,
+                        farmId = t.farmId,
+                        timestamp = t.timestamp,
+                        windSpeed = t.windSpeed,
+                        windDirection = t.windDirection,
+                        ambientTemperatur = t.ambientTemperatur,
+                        rotorSpeed = t.rotorSpeed,
+                        powerOutput = t.powerOutput,
+                        nacelleDirection = t.nacelleDirection,
+                        bladePitch = t.bladePitch,
+                        generatorTemp = t.generatorTemp,
+                        gearboxTemp = t.gearboxTemp,
+                        vibration = t.vibration,
+                        status = t.status
+                    })
+                    .ToListAsync()
+        );
+
+        // Initial data for the UI
+        var initialData = await _ctx.Telemetries
+            .OrderByDescending(t => t.timestamp)
+            .Take(50)
+            .Select(t => new WindmillTelemetryDTO
+            {
+                turbineId = t.turbineId,
+                turbineName = t.turbineName,
+                farmId = t.farmId,
+                timestamp = t.timestamp,
+                windSpeed = t.windSpeed,
+                windDirection = t.windDirection,
+                ambientTemperatur = t.ambientTemperatur,
+                rotorSpeed = t.rotorSpeed,
+                powerOutput = t.powerOutput,
+                nacelleDirection = t.nacelleDirection,
+                bladePitch = t.bladePitch,
+                generatorTemp = t.generatorTemp,
+                gearboxTemp = t.gearboxTemp,
+                vibration = t.vibration,
+                status = t.status
+            })
+            .ToListAsync();
+
+        return new RealtimeListenResponse<List<WindmillTelemetryDTO>>(group, initialData);
+    }
+
+    [HttpPost(nameof(AddTelemetry))]
+    public async Task<IActionResult> AddTelemetry([FromBody] WindmillTelemetryDTO telemetry)
     {
         var entity = new WindmillTelemetryEntity
         {
@@ -36,9 +112,8 @@ public class WindmillController(ISseBackplane backplane, WindmillDbContext _ctx,
             vibration = telemetry.vibration,
             status = telemetry.status
         };
-
-        _context.Telemetries.Add(entity);
-        await _context.SaveChangesAsync();
+        _ctx.Telemetries.Add(entity);
+        await _ctx.SaveChangesAsync();
 
         return Ok();
     }
---
+++ Server/Controllers/WindmillMqttController.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+using Mqtt.Controllers;
+using Server.Entities;
+using Server.Services;
+
+
+namespace Server.Controllers;
+
+
+public class WindmillMqttController(ILogger<WindmillMqttController> logger, WindmillDbContext ctx)
+{
+
+    [MqttRoute("station/aaa/sensor/{turbineId}/telemetry")]
+    public async Task ListenForTelemetries(WindmillTelemetryEntity telemetry, string turbineId)
+    {
+        logger.LogInformation(JsonSerializer.Serialize(telemetry));
+        telemetry.Id = Guid.NewGuid();
+        ctx.Telemetries.Add(telemetry);
+        await ctx.SaveChangesAsync();
+    }
+}
