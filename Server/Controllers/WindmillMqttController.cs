35377edeee9d2b1590ba7b2d48b5642d805f5806
DKHacher <146067965+DKHacher@users.noreply.github.com>
2026-03-02 11:10:39 +0100

    changed stuff on controllers

--- Server/Controllers/WindmillMqttController.cs
+++ Server/Controllers/WindmillMqttController.cs
@@ -10,7 +10,7 @@ namespace Server.Controllers;
 public class WindmillMqttController(ILogger<WindmillMqttController> logger, WindmillDbContext ctx)
 {
 
-    [MqttRoute("station/aaa/sensor/{turbineId}/telemetry")]
+    [MqttRoute("farm/WindmillFarm/windmill/{turbineId}/telemetry")]
     public async Task ListenForTelemetries(WindmillTelemetryEntity telemetry, string turbineId)
     {
         logger.LogInformation(JsonSerializer.Serialize(telemetry));
@@ -18,4 +18,21 @@ public class WindmillMqttController(ILogger<WindmillMqttController> logger, Wind
         ctx.Telemetries.Add(telemetry);
         await ctx.SaveChangesAsync();
     }
+    [MqttRoute("farm/WindmillFarm/windmill/{turbineId}/alert")]
+    public async Task ListenForAlerts(WindmillAlertEntity alert, string turbineId)
+    {
+        logger.LogInformation(JsonSerializer.Serialize(alert));
+        alert.Id = Guid.NewGuid();
+        ctx.Alerts.Add(alert);
+        await ctx.SaveChangesAsync();
+    }
+    
+    /*[MqttRoute("farm/WindmillFarm/windmill/{turbineId}/command")] //TODO: missing command entity
+    public async Task CommandMqtt(WindmillAlertEntity alert, string turbineId)
+    {
+        logger.LogInformation(JsonSerializer.Serialize(alert));
+        alert.Id = Guid.NewGuid();
+        ctx.Alerts.Add(alert);
+        await ctx.SaveChangesAsync();
+    }*/
 }
