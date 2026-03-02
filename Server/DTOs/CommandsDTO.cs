1656e9b3ec8815dc9678c680de6e04c01a5407ee
David Ch <raulchuchuca951@gmail.com>
2026-03-02 11:09:07 +0100

    Updated Entities & DTOs

--- Server/DTOs/CommandsDTO.cs
+++ Server/DTOs/CommandsDTO.cs
@@ -2,5 +2,9 @@
 
 public class CommandsDTO
 {
-    
+    public int userId { get; set; }
+    public string turbineId { get; set; }
+    public string action { get; set; }
+    public string args { get; set; }
+    public string issuedAt { get; set; }
 }
--- Server/Entities/User.cs
+++ Server/Entities/User.cs
@@ -2,5 +2,8 @@
 
 public class User
 {
-    
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Password { get; set; }
+    public string role { get; set; }
 }
