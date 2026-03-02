5cc68404d9ce29da1460925dba9ab0a1eb740940
David Ch <raulchuchuca951@gmail.com>
2026-03-02 11:04:02 +0100

    Entities & DTOs Commands and Entities implemented

---
+++ Server/DTOs/UserDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Server.DTOs;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Password { get; set; }
+    public string role { get; set; }
+}
