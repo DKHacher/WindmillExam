ea88ed4f9809ebda35885ac1f657480d86b53eea
thoknu <144660048+thoknu@users.noreply.github.com>
2026-03-05 11:21:48 +0100

    Changed DTO reference to Entity ref

--- Client/src/components/TurbineDetails.tsx
+++ Client/src/components/TurbineDetails.tsx
@@ -1,7 +1,7 @@
-﻿import {WindmillTelemetryDTO} from "../services/generated-ts-client.ts";
+﻿import {WindmillTelemetryEntity} from "../services/generated-ts-client.ts";
 
 type TurbineDetailsProps = {
-    turbine: WindmillTelemetryDTO;
+    turbine: WindmillTelemetryEntity;
     onClose: () => void;
 };
 
