0741d4177db6b7bbf0e8bae8d09e51298d39545a
DKHacher <146067965+DKHacher@users.noreply.github.com>
2026-03-05 08:43:22 +0100

    working on updating controllers and added new migrations for db

--- Server/Controllers/WindmillMqttController.cs
+++ Server/Controllers/WindmillMqttController.cs
@@ -7,8 +7,12 @@ using Server.Services;
 namespace Server.Controllers;
 
 
-public class WindmillMqttController(ILogger<WindmillMqttController> logger, WindmillDbContext ctx)
+public class WindmillMqttController(
+    ILogger<WindmillMqttController> logger,
+    WindmillDbContext ctx,
+    IMqttClientService mqttService)
 {
+    private readonly IMqttClientService mqttService = mqttService; // <-- inject the singleton
 
     [MqttRoute("farm/WindmillFarm/windmill/{turbineId}/telemetry")]
     public async Task ListenForTelemetries(WindmillTelemetryEntity telemetry, string turbineId)
@@ -27,12 +31,40 @@ public class WindmillMqttController(ILogger<WindmillMqttController> logger, Wind
         await ctx.SaveChangesAsync();
     }
     
-    /*[MqttRoute("farm/WindmillFarm/windmill/{turbineId}/command")] //TODO: missing command entity
-    public async Task CommandMqtt(WindmillAlertEntity alert, string turbineId)
+    [MqttRoute("farm/WindmillFarm/windmill/{turbineId}/command")]
+    public async Task CommandMqtt(Commands command, string turbineId)
     {
-        logger.LogInformation(JsonSerializer.Serialize(alert));
-        alert.Id = Guid.NewGuid();
-        ctx.Alerts.Add(alert);
+        // Assign ID and timestamp
+        command.id = Guid.NewGuid();
+        command.turbineId = turbineId;
+        command.issuedAt = DateTime.UtcNow.ToString("o");
+
+        logger.LogInformation("Command received: {Command}", JsonSerializer.Serialize(command));
+
+        // Validate action
+        var allowedActions = new[] { "Start", "Stop", "Set blade pitch", "Set report interval" };
+        if (!allowedActions.Contains(command.action))
+        {
+            logger.LogWarning("Invalid command action: {Action}", command.action);
+            return;
+        }
+
+        // Publish to MQTT
+        if (mqttService.IsConnected)
+        {
+            var topic = $"farm/WindmillFarm/windmill/{turbineId}/command";
+            var payload = JsonSerializer.Serialize(new { action = command.action, args = command.args });
+
+            await mqttService.PublishAsync(topic, payload);
+            logger.LogInformation("Command published to MQTT topic {Topic}", topic);
+        }
+        else
+        {
+            logger.LogWarning("MQTT client is not connected!");
+        }
+
+        // Save to database
+        ctx.Commands.Add(command);
         await ctx.SaveChangesAsync();
-    }*/
+    }
 }
