8fff7b759eb9ae360477f17251d89411d01e3336
David Ch <raulchuchuca951@gmail.com>
2026-03-05 10:47:23 +0100

    generated-ts-client updated

--- Client/src/services/generated-ts-client.ts
+++ Client/src/services/generated-ts-client.ts
@@ -91,6 +91,40 @@ export class WindmillClient {
         return Promise.resolve<RealtimeListenResponseOfListOfWindmillAlertEntity>(null as any);
     }
 
+    startTurbine(connectionId: string | undefined): Promise<void> {
+        let url_ = this.baseUrl + "/StartTurbine?";
+        if (connectionId === null)
+            throw new globalThis.Error("The parameter 'connectionId' cannot be null.");
+        else if (connectionId !== undefined)
+            url_ += "connectionId=" + encodeURIComponent("" + connectionId) + "&";
+        url_ = url_.replace(/[?&]$/, "");
+
+        let options_: RequestInit = {
+            method: "POST",
+            headers: {
+            }
+        };
+
+        return this.http.fetch(url_, options_).then((_response: Response) => {
+            return this.processStartTurbine(_response);
+        });
+    }
+
+    protected processStartTurbine(response: Response): Promise<void> {
+        const status = response.status;
+        let _headers: any = {}; if (response.headers && response.headers.forEach) { response.headers.forEach((v: any, k: any) => _headers[k] = v); };
+        if (status === 200) {
+            return response.text().then((_responseText) => {
+            return;
+            });
+        } else if (status !== 200 && status !== 204) {
+            return response.text().then((_responseText) => {
+            return throwException("An unexpected server error occurred.", status, _responseText, _headers);
+            });
+        }
+        return Promise.resolve<void>(null as any);
+    }
+
     connect(): Promise<void> {
         let url_ = this.baseUrl + "/sse";
         url_ = url_.replace(/[?&]$/, "");
