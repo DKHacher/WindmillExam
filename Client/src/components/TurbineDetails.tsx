b93c8cda30bc9d953aca80031fcfb9e019343976
thoknu <144660048+thoknu@users.noreply.github.com>
2026-03-09 09:55:24 +0100

    Added bladepitch command

--- Client/src/components/TurbineDetails.tsx
+++ Client/src/components/TurbineDetails.tsx
@@ -3,18 +3,18 @@ import {restClient} from "../services/sse.ts";
 import {useState} from "react";
 
 type TurbineDetailsProps = {
-    turbine: WindmillTelemetryEntity;
+    turbine: WindmillTelemetryEntity
     onClose: () => void;
 };
 
 function TurbineDetails({ turbine, onClose }: TurbineDetailsProps) {
 
-    const [stopReason, setStopReason] = useState<string>();
-    const [bladePitch, setBladePitch] = useState<number>();
+    const [stopReason, setStopReason] = useState<string>("");
+    const [bladePitch, setBladePitch] = useState<number | undefined>();
 
     const start = () => restClient.startTurbine(turbine.turbineId);
     const stop = () => restClient.stopTurbine(turbine.turbineId, stopReason);
-    const setBladePitch = () => restClient.setBladePitch(turbine.turbineId, bladePitch);
+    const setBladePitchCommand = () => restClient.setBladePitch(turbine.turbineId, bladePitch);
 
     return (
         <div
@@ -57,6 +57,14 @@ function TurbineDetails({ turbine, onClose }: TurbineDetailsProps) {
                 value={stopReason}
                 placeholder="Enter reason for stopping"
                 onChange={(e) => setStopReason(e.target.value)}
+            />
+            <p>Adjust blade pitch</p>
+            <button onClick={setBladePitchCommand}></button>
+            <input
+                type="number"
+                value={bladePitch}
+                placeholder="Enter new blade pitch"
+                onChange={(e) => setBladePitch(Number(e.target.value))}
             />
         </div>
     );
