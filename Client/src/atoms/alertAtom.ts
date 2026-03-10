d4a2d27da7baa12ecfd86f5f032d7d070b7bd5be
thoknu <144660048+thoknu@users.noreply.github.com>
2026-03-10 23:49:39 +0100

    Added: SSE subscription to alerts with toasting

---
+++ Client/src/atoms/alertAtom.ts
@@ -0,0 +1,4 @@
+﻿import {WindmillAlertEntity} from "../services/generated-ts-client.ts";
+import {atom} from "jotai";
+
+export const alertAtom = atom<WindmillAlertEntity[]>([])
--- Client/src/components/App.tsx
+++ Client/src/components/App.tsx
@@ -2,18 +2,23 @@ import Dashboard from "./Dashboard.tsx";
 import '../animation.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Login.tsx";
+import {Toaster} from "react-hot-toast";
 // import PrivateRoute from "./PrivateRoute.tsx";
 
 function App() {
     // const [loggedIn, setLoggedIn] = useState(false);
 
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/dashboard" element={<Dashboard />} />
-            </Routes>
-        </BrowserRouter>
+        <>
+            <Toaster position="top-right" />
+
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Login />} />
+                    <Route path="/dashboard" element={<Dashboard />} />
+                </Routes>
+            </BrowserRouter>
+        </>
     );
 }
 
